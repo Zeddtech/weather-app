8a9ab4adf7a5870e8fa6db84b0bb9d47cea214ed
detrinity27 <detrinity27@gmail.com>
2022-10-14 22:55:54 +0100

    optimizatiom

--- js/main.js
+++ js/main.js
@@ -1,9 +1,5 @@
-import {
-  countries
-} from "./vendor/countries.js";
-import {
-  key
-} from './config.js';
+import { countries } from "./vendor/countries.js";
+import { key } from "./config.js";
 const currentLocation = document.querySelector(".header");
 const parameters = document.querySelector(".parameters");
 const sun = document.querySelector(".sun-box");
@@ -11,38 +7,9 @@ const daily = document.querySelector(".days");
 const hourly = document.querySelector(".hours");
 const input = document.querySelector(".input-search");
 const slider = document.querySelectorAll(".slider");
-const weatherImg = document.querySelector(".weather-app");
-
+const weatherApp = document.querySelector(".weather-app");
+const video = document.querySelector("video");
 getLocation();
-
-
-// hourl and daily carousel
-let isDown = false;
-let startX;
-let scrollLeft;
-slider.forEach((slider) => {
-  slider.addEventListener("mousedown", (e) => {
-    isDown = true;
-    // get the click point of the mouse
-    startX = e.pageX - slider.offsetLeft;
-    // get the lenght the mouse of drag to
-    scrollLeft = slider.scrollLeft;
-  });
-  slider.addEventListener("mouseleave", () => (isDown = false));
-  slider.addEventListener("mouseup", () => (isDown = false));
-  slider.addEventListener("mousemove", (e) => {
-    if (!isDown) return;
-    e.preventDefault();
-    const x = e.pageX - slider.offsetLeft;
-    const walk = (x - startX) * 2; //scroll-fast
-    slider.scrollLeft = scrollLeft - walk;
-  });
-  slider.addEventListener("wheel",
-    (evt) => {
-      evt.preventDefault();
-      slider.scrollLeft += evt.deltaY/2;
-    });
-});
 // round to the nearest integer
 function f(int) {
   return Math.round(int);
@@ -53,37 +20,40 @@ function getLocation() {
     var options = {
       timeout: 10000,
       enableHighAccuracy: true,
-      maximumAge: 2000
+      maximumAge: 2000,
     };
-    navigator.geolocation.getCurrentPosition(successHandler, errorHandler, options);
+    navigator.geolocation.getCurrentPosition(
+      successHandler,
+      errorHandler,
+      options
+    );
   } else {
-    alert(" unable to get your geolocation information, your browser does not support geolocation! to get your current position weather information click on the search icon then search for your city name ");
-    getCityInfo(6.605874, 3.349149)
+    alert(
+      " unable to get your geolocation information, your browser does not support geolocation! to get your current position weather information click on the search icon then search for your city name "
+    );
+    getCityInfo(6.605874, 3.349149);
   }
-
 }
 function successHandler(location) {
-  let {
-    latitude,
-    longitude
-  } = location.coords;
+  let { latitude, longitude } = location.coords;
   // fetch current user city info
   getCityInfo(latitude, longitude);
 }
 function errorHandler(err) {
   if (err.code == 1) {
-    alert("Error: Unable to get your geolocation information  because the page didn't have the permission to do it. please turn on your device  location and grant acess to location to get your weather information or You can  search city names to get it's current weather information");
+    alert(
+      "Error: Unable to get your geolocation information  because the page didn't have the permission to do it. please turn on your device  location and grant acess to location to get your weather information or You can  search city names to get it's current weather information"
+    );
   } else if (err.code == 2) {
     alert("Error: your geolocation lnformation is  unavailable!");
   } else if (err.code == 3) {
     alert("Error Timeout: Failed to get  geolocation lnformation !");
   }
-  getCityInfo(6.605874, 3.349149)
+  getCityInfo(6.605874, 3.349149);
 }
 
-
-
 async function getCityInfo(lat, long) {
+  weatherApp.style.opacity = 0;
   try {
     let res = await fetch(
       `https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${long}&limit=1&appid=${key}`
@@ -97,31 +67,25 @@ async function getCityInfo(lat, long) {
       fetchWeather(res[0]);
     }
   } catch (error) {
-    weatherImg.style.opacity = 1;
+    weatherApp.style.opacity = 1;
     alert(error);
   }
 }
 
 function fetchWeather(coords) {
-  let {
-    name,
-    lat,
-    lon,
-    state,
-    country
-  } = coords;
+  let { name, lat, lon, state, country } = coords;
   fetch(
     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=alerts,minutely&appid=${key}`
   )
-  .then((data) => data.json())
-  .then((data) => {
-    if (data.message) throw new Error(data.message);
-    render(name, state, country, data);
-  })
-  .catch((err) => {
-    weatherImg.style.opacity = 1;
-    alert("Bad Request, " + err);
-  });
+    .then((data) => data.json())
+    .then((data) => {
+      if (data.message) throw new Error(data.message);
+      render(name, state, country, data);
+    })
+    .catch((err) => {
+      weatherApp.style.opacity = 1;
+      alert("Bad Request, " + err);
+    });
 }
 
 // search city
@@ -147,44 +111,44 @@ cities.forEach((city) => {
 });
 
 function getcitycords(city) {
-  weatherImg.style.opacity = 0;
+  weatherApp.style.opacity = 0;
   fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
   )
-  .then((data) => data.json())
-  .then((res) => {
-    if (res.cod != 200) {
-      throw new Error(res.message);
-    } else {
-      getCityInfo(res.coord.lat, res.coord.lon);
-    }
-  })
-  .catch((err) => {
-    weatherImg.style.opacity = 1;
-    alert(err);
-  });
+    .then((data) => data.json())
+    .then((res) => {
+      if (res.cod != 200) {
+        throw new Error(res.message);
+      } else {
+        getCityInfo(res.coord.lat, res.coord.lon);
+      }
+    })
+    .catch((err) => {
+      weatherApp.style.opacity = 1;
+      alert(err);
+    });
 }
 
 // render to the browser
 function render(cityname, state, country, weatherInfo) {
   let date = moment
-  .unix(weatherInfo.current.dt)
-  .format(" dddd MMMM Do, h:mm A");
+    .unix(weatherInfo.current.dt)
+    .format(" dddd MMMM Do, h:mm A");
   let hour = "";
   let day = "";
   let weather = weatherInfo.current.weather[0].main;
   currentLocation.innerHTML = ` <div class="city">${cityname} , ${
-  state ? state + ",": ""
-  } ${country ? countries[country]: ""}</div>
+    state ? state + "," : ""
+  } ${country ? countries[country] : ""}</div>
   <div class="weather">
   <h1 class="current-temp" title="Temperature">${f(
     weatherInfo.current.temp
   )}&deg;</h1>
   <div class="weather-condition">
   <img src="https://openweathermap.org/img/wn/${
-  weatherInfo.current.weather[0].icon
+    weatherInfo.current.weather[0].icon
   }@2x.png" alt="${weatherInfo.current.weather[0].description}" title="${
-  weatherInfo.current.weather[0].description
+    weatherInfo.current.weather[0].description
   }">
   <p >${weather}</p>
   </div>
@@ -201,9 +165,7 @@ function render(cityname, state, country, weatherInfo) {
   <i class="bx bx-droplet"></i>
   <div class="param-body">
   <p class="param-name">humidity</p>
-  <p class="param-value">${f(
-    weatherInfo.current.humidity
-  )}<small>%</small></p>
+  <p class="param-value">${f(weatherInfo.current.humidity)}<small>%</small></p>
   </div>
   </div>
   <!-- pressure -->
@@ -230,14 +192,14 @@ function render(cityname, state, country, weatherInfo) {
   sun.innerHTML = `<div class="sun-rise-con">
   <div class="sun-rise" title="Sun-rise"></div>
   <p class="sun-rise">${moment
-  .unix(weatherInfo.current.sunrise)
-  .format("h:mm A")}</p>
+    .unix(weatherInfo.current.sunrise)
+    .format("h:mm A")}</p>
   </div>
   <div class="sun-set-con">
   <div class="sun-set" title="sun-set"></div>
   <p class="sun-set">${moment
-  .unix(weatherInfo.current.sunset)
-  .format("h:mm A")}</p>
+    .unix(weatherInfo.current.sunset)
+    .format("h:mm A")}</p>
   </div>`;
   // hourly forecast
   weatherInfo.hourly.forEach((el, indx) => {
@@ -245,9 +207,9 @@ function render(cityname, state, country, weatherInfo) {
       hour += `<div class="hourly">
       <p class="time">${moment.unix(el.dt).format("h:mm A")}</p>
       <img src="https://openweathermap.org/img/wn/${
-      el.weather[0].icon
+        el.weather[0].icon
       }@2x.png" alt="${el.weather[0].description}" title="${
-      el.weather[0].description
+        el.weather[0].description
       }" />
       <p class="temp" title="Temperature">${f(el.temp)}&deg;</p>
       </div>`;
@@ -261,13 +223,15 @@ function render(cityname, state, country, weatherInfo) {
       <div class="daily">
       <p class="day">${moment.unix(el.dt).format("ddd")}</p>
       <img src="https://openweathermap.org/img/wn/${
-      el.weather[0].icon
+        el.weather[0].icon
       }@2x.png" alt="${el.weather[0].description}" title="${
-      el.weather[0].description
+        el.weather[0].description
       }"/>
       <p class="temp" title='temperature'>${f(el.temp.day)}&deg;</p>
       <p class="temp" title='humidity'>${f(el.humidity)}<small>%</small></p>
-      <p class="temp" title='wind speed'>${f(el.wind_speed)}<small>m/s</small></p>
+      <p class="temp" title='wind speed'>${f(
+        el.wind_speed
+      )}<small>m/s</small></p>
       </div>`;
     }
   });
@@ -275,28 +239,55 @@ function render(cityname, state, country, weatherInfo) {
   // switch background
   switch (weather) {
     case "Thunderstorm":
-      weatherImg.style.backgroundImage = "url(../img/thunderstorm.gif)";
+      video.src = "./img/thunderstorm.mp4";
       break;
     case "Drizzle":
     case "Rain":
-      weatherImg.style.backgroundImage = "url(../img/rain.gif)";
+      video.src = "./img/rain.mp4";
       break;
     case "Snow":
-      weatherImg.style.backgroundImage = "url(../img/snow.gif)";
+      video.src = "./img/snow.mp4";
       break;
     case "Mist":
     case "Fog":
     case "Smoke":
     case "Haze":
-      weatherImg.style.backgroundImage = "url(../img/fog.gif)";
+      video.src = "./img/fog.mp4";
       break;
     case "Clear":
-      weatherImg.style.backgroundImage = "url(../img/clear.gif)";
+      video.src = "./img/clear.mp4";
       break;
 
     default:
-      weatherImg.style.backgroundImage = "url(./img/clouds.gif)";
+      video.src = "./img/clouds.mp4";
       break;
   }
-  weatherImg.style.opacity = 1;
+  weatherApp.style.opacity = 1;
 }
+
+// hourl and daily carousel
+let isDown = false;
+let startX;
+let scrollLeft;
+slider.forEach((slider) => {
+  slider.addEventListener("mousedown", (e) => {
+    isDown = true;
+    // get the click point of the mouse
+    startX = e.pageX - slider.offsetLeft;
+    // get the lenght the mouse of drag to
+    scrollLeft = slider.scrollLeft;
+  });
+  slider.addEventListener("mouseleave", () => (isDown = false));
+  slider.addEventListener("mouseup", () => (isDown = false));
+  slider.addEventListener("mousemove", (e) => {
+    if (!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft;
+    const walk = (x - startX) * 2; //scroll-fast
+    slider.scrollLeft = scrollLeft - walk;
+  });
+  slider.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    slider.scrollLeft += evt.deltaY / 2;
+  });
+});
